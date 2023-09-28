0dbf94e40493994f0c926fe069f8884331354cdb
Murilo Molina <murilo.m.barone@gmail.com>
2023-09-28 13:03:54 -0300

    encontro04(dart-flutter): Imagens + StatefulWidget

---
+++ lib/my_app.dart
@@ -0,0 +1,14 @@
+import 'package:add_image/my_home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
